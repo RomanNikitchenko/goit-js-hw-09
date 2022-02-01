13af23665b5e7db8a74c8ad8a66f700eed23df4d
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-01 17:04:03 +0200

    ht 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+console.log(btnStop);
+
+console.log(getRandomHexColor());
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
