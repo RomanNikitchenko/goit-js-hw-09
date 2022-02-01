27a683bdf8f8c6798df8f548c79454665037b6ba
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-01 21:36:19 +0200

    ht 9 t1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,33 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
 
-console.log(btnStop);
+let timerId = null;
+let disabled = null;
 
-console.log(getRandomHexColor());
+btnStart.addEventListener("click", onRandomBackgroundColor);
+
+function onRandomBackgroundColor() {
+  if (disabled) {
+    return;
+  };
+
+  disabled = true;
+  btnStart.setAttribute("disabled", "disabled");
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+btnStop.addEventListener("click", offRandomBackgroundColor);
+
+function offRandomBackgroundColor() {
+  btnStart.removeAttribute("disabled");
+  disabled = false;
+  clearInterval(timerId);
+};
