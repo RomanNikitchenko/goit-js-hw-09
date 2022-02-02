7d8fb8e639556fc7820781c0f5b778fcb8eb4e16
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-03 00:03:13 +0200

    ht9 t 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,7 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 
-const datetimePicker = document.querySelector("#datetime-picker");
+const dateTimePicker = document.querySelector("#datetime-picker");
 const dataStart = document.querySelector('[data-start]');
 
 const sdays = document.querySelector('[data-days]');
@@ -21,4 +21,61 @@ const options = {
   },
 };
 
-flatpickr(datetimePicker, options);
+flatpickr(dateTimePicker, options);
+
+dataStart.setAttribute("disabled", "disabled");
+
+dateTimePicker.addEventListener('input', () => {
+  const appointedTime = new Date(dateTimePicker.value);
+  const subtractTime = appointedTime - new Date();
+
+  if (subtractTime < 0) {
+    window.alert("Please choose a date in the future")
+    return
+  }
+
+  dataStart.removeAttribute("disabled");
+});
+
+
+let timerId = null;
+let disabled = false;
+
+dataStart.addEventListener('click', () => {
+
+  if (disabled) {
+    return;
+  };
+  disabled = true;
+
+  const appointedTime = new Date(dateTimePicker.value);
+
+  timerId = setInterval(() => {
+    const subtractTime = appointedTime - new Date();
+
+    if (subtractTime <= 0) {
+      clearInterval(timerId);
+      disabled = false;
+      return
+    }
+
+    console.log(convertMs(subtractTime));
+  }, 1000);
+
+});
+
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
